f0c012028ca5ea012f9046bf15c8206d367aa5f9
jonathansayer <jon.sayer1@gmail.com>
2015-09-01 13:56:27 +0100

    refactored gameSpec

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,6 +1,6 @@
 describe('game', function()
 {
-  
+
   describe('A Game',function() {
     beforeEach(function(){
     game1 = new game
@@ -11,97 +11,88 @@ describe('game', function()
       expect(game1.frames[0].total).toBe(5);
     });
 
-    it(' will allow a user to switch to the next frame to add scores.', function() {
-      game1.addScore(4);
-      game1.addScore(5);
-      game1.addScore(6);
-      game1.addScore(7);
-      expect(game1.frames[0].total).toBe(9);
-      expect(game1.frames[1].total).toBe(13);
-    })
-
-    it(' will add the next score of the next frame if a strike occurs', function() {
-      game1.addScore(4);
-      game1.addScore(5);
-      game1.addScore(10);
-      game1.addScore(6);
-      game1.addScore(7);
-      expect(game1.frames[1].total).toBe(23);
-    })
-
-    it(' two strikes in a row will add to additional points in the previoue frames',function(){
-    game1.addScore(10);
-    game1.addScore(10);
-    game1.addScore(9);
-    game1.addScore(0);
-    expect(game1.frames[0].total).toBe(29);
-    expect(game1.frames[1].total).toBe(19);
-    expect(game1.frames[2].total).toBe(9);
-  })
-
-    it('a frame with a spare will have additional points added equal to the next ball', function(){
-      game1.addScore(9);
-      game1.addScore(1);
-      game1.addScore(4);
-      game1.addScore(3);
-      expect(game1.frames[0].spare).toBe(true);
-      expect(game1.frames[0].total).toBe(14);
-    })
 
     it('once a frame is full, the next frame is automatically filled next',function(){
-      game1.addScore(3);
-      game1.addScore(4);
+      fillFrameWith(4,5);
       game1.addScore(5);
-      expect(game1.frames[0].total).toBe(7);
+      expect(game1.frames[0].total).toBe(9);
       expect(game1.frames[1].total).toBe(5);
     })
 
-    it('a full game can be played', function(){
-      game1.addScore(10);
-      game1.addScore(6);
-      game1.addScore(3);
-      game1.addScore(7);
-      game1.addScore(3);
-      game1.addScore(5);
-      game1.addScore(3);
-      game1.addScore(9);
-      game1.addScore(1);
-      game1.addScore(3);
-      game1.addScore(0);
-      game1.addScore(0);
-      game1.addScore(2);
-      game1.addScore(5);
-      game1.addScore(4);
-      game1.addScore(10);
-      game1.addScore(5);
-      game1.addScore(5);
-      game1.addScore(10);
+    describe('when a strike is scored', function(){
+
+      it(' will add the next score of the next frame if a strike occurs', function() {
+        fillFrameWith(4,5);
+        scoreStrike();
+        fillFrameWith(6,7);
+        expect(game1.frames[1].total).toBe(23);
+      })
+
+
+      it(' two strikes in a row will add to additional points in the previoue frames',function(){
+      scoreStrike();
+      scoreStrike();
+      fillFrameWith(4,5);
+      expect(game1.frames[0].total).toBe(29);
+      expect(game1.frames[1].total).toBe(19);
+      expect(game1.frames[2].total).toBe(9);
+    })
+  })
+
+  describe('when a spare is scored', function(){
+
+      it('a frame with a spare will have additional points added equal to the next ball', function(){
+        scoreSpare(9,1);
+        fillFrameWith(4,5);
+        expect(game1.frames[0].spare).toBe(true);
+        expect(game1.frames[0].total).toBe(14);
+      })
+  })
+
+    it('a full game can be played where the the final three frames are strike, spare, strike and a bonus roll needs to be initiated', function(){
+      scoreStrike();
+      fillFrameWith(6,3);
+      scoreSpare(7,3);
+      fillFrameWith(5,3);
+      scoreSpare(9,1);
+      fillFrameWith(3,0);
+      fillFrameWith(0,2);
+      fillFrameWith(5,4);
+      scoreStrike();
+      scoreSpare(5,5);
+      scoreStrike();
       game1.totalCalc();
       expect(game1.gameTotal).toBe(128);
     })
 
  it('a second full game can be played', function(){
-      game1.addScore(10);
-      game1.addScore(6);
-      game1.addScore(3);
-      game1.addScore(7);
-      game1.addScore(3);
-      game1.addScore(5);
-      game1.addScore(3);
-      game1.addScore(9);
-      game1.addScore(1);
-      game1.addScore(3);
-      game1.addScore(0);
-      game1.addScore(0);
-      game1.addScore(2);
-      game1.addScore(5);
-      game1.addScore(4);
-      game1.addScore(10);
-      game1.addScore(5);
-      game1.addScore(4);
+      scoreStrike();
+      fillFrameWith(6,3);
+      scoreSpare(7,3);
+      fillFrameWith(5,3);
+      scoreSpare(9,1);
+      fillFrameWith(3,0);
+      fillFrameWith(0,2);
+      fillFrameWith(5,4);
+      scoreStrike();
+      fillFrameWith(5,4);
       game1.totalCalc();
       expect(game1.gameTotal).toBe(106);
     })
+
+    var fillFrameWith = function(score1, score2){
+      game1.addScore(score1);
+      game1.addScore(score2);
+    }
+
+    var scoreStrike = function(){
+      game1.addScore(10);
+    }
+
+    var scoreSpare = function(score1, score2) {
+      game1.addScore(score1);
+      game1.addScore(score2);
+    }
 
 
 
