de74cf160198bc87d9fd1ce4fe6462221b71e1ac
jonathansayer <jon.sayer1@gmail.com>
2015-09-01 15:59:07 +0100

    refactored frameSpec

--- spec/helper.js
+++ spec/helper.js
@@ -1,13 +1,22 @@
-    var fillFrameWith = function(score1, score2){
-      game1.addScore(score1);
-      game1.addScore(score2);
-    }
+var fillFrameWith = function(score1, score2){
+  game1.addScore(score1);
+  game1.addScore(score2);
+}
 
-    var scoreStrike = function(){
-      game1.addScore(10);
-    }
+var scoreStrike = function(){
+  game1.addScore(10);
+}
 
-    var scoreSpare = function(score1, score2) {
-      game1.addScore(score1);
-      game1.addScore(score2);
-    }
+var scoreSpare = function(score1, score2) {
+  game1.addScore(score1);
+  game1.addScore(score2);
+}
+
+var enterScores = function(score1, score2) {
+  frame1.scoreInput(score1);
+  frame1.scoreInput(score2);
+}
+
+var strikeShot = function() {
+  frame1.scoreInput(10);
+}
