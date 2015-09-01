05087917c79636ad551122931671e3b67ada0b7b
jonathansayer <jon.sayer1@gmail.com>
2015-09-01 14:44:38 +0100

    refactor code

--- src/public/javascripts/game.js
+++ src/public/javascripts/game.js
@@ -22,7 +22,7 @@ var game = function() {
     var prevFrame = frameNumber-1;
     if(prevFrame >= 0){
       if(this.frames[frameNumber].strike == false && this.frames[frameNumber].full && this.frames[prevFrame].strike == true){
-      
+
         for(n = frameNumber; n > 0 ; n--){
           if(this.frames[n-1].strike == false) {break;}
           this.frames[n-1].total += this.frames[n].total
@@ -31,7 +31,7 @@ var game = function() {
       }
 
       if(this.frames[prevFrame].spare == true && this.frames[frameNumber].full){
-        this.frames[prevFrame].total += this.frames[frameNumber].scores[0]     
+        this.frames[prevFrame].total += this.frames[frameNumber].scores[0]
       }
       }
     if(this.frames[frameNumber].full){
@@ -44,7 +44,7 @@ var game = function() {
       this.gameTotal += this.frames[n].total
     }
   };
-  
+
 
   var nextFrame = function() {
     frameNumber += 1;
@@ -55,8 +55,4 @@ var game = function() {
     frameNumber -= 1;
     return "Frame Number " + (frameNumber + 1);
   };
-
-  
-
-
 };
