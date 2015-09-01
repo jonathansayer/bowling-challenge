57b25dd68657efa0d0ea3b721e77f798a61de42d
jonathansayer <jon.sayer1@gmail.com>
2015-09-01 16:07:49 +0100

    changed 'frame' to 'testFrame'

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,50 +1,50 @@
 describe('bowlingScore', function (){
 
   beforeEach(function(){
-    frame1 = new frame();
+    testFrame = new frame();
     });
 
   describe('a frame',function(){
 
     it('can keep track of a score that isnt a strike',function(){
-      frame1.scoreInput(3);
-      expect(frame1.total).toBe(3);
+      testFrame.scoreInput(3);
+      expect(testFrame.total).toBe(3);
     });
 
     it('can keep track of multiple scores', function() {
       enterScores(3,6);
-      expect(frame1.total).toBe(9);
+      expect(testFrame.total).toBe(9);
     });
 
     it('returns "strike" when a score of 10 is entered', function(){
-      expect(frame1.scoreInput(10)).toBe("strike!");
-      expect(frame1.total).toBe(10);
+      expect(testFrame.scoreInput(10)).toBe("strike!");
+      expect(testFrame.total).toBe(10);
     });
 
     it('has a strike status when a stike is scored', function() {
-      frame1.scoreInput(10);
-      expect(frame1.strike).toBe(true);
+      testFrame.scoreInput(10);
+      expect(testFrame.strike).toBe(true);
     });
 
     it('wont allow any more than two scores to be entered', function(){
       enterScores(4,5);
-      expect( function(){ frame1.scoreInput(6); } ).toThrow(new Error("The Frame is Full"));
+      expect( function(){ testFrame.scoreInput(6); } ).toThrow(new Error("The Frame is Full"));
     });
 
     it('records the scores in the frame', function() {
       enterScores(4,5);
-      expect(frame1.scores).toEqual([4,5]);
+      expect(testFrame.scores).toEqual([4,5]);
     });
 
     it ('is full if a strike occurs', function() {
       strikeShot();
-      expect(frame1.full).toBe(true);
+      expect(testFrame.full).toBe(true);
     })
 
     it(' if all pins are knocked down in two balls, the spare status is true', function(){
       enterScores(9,1);
-      expect(frame1.strike).toBe(false);
-      expect(frame1.spare).toBe(true);
+      expect(testFrame.strike).toBe(false);
+      expect(testFrame.spare).toBe(true);
     })
   });
 });
--- spec/helper.js
+++ spec/helper.js
@@ -13,10 +13,10 @@ var scoreSpare = function(score1, score2) {
 }
 
 var enterScores = function(score1, score2) {
-  frame1.scoreInput(score1);
-  frame1.scoreInput(score2);
+  testFrame.scoreInput(score1);
+  testFrame.scoreInput(score2);
 }
 
 var strikeShot = function() {
-  frame1.scoreInput(10);
+  testFrame.scoreInput(10);
 }
